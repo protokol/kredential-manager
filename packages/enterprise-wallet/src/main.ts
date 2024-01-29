0555f2e790bed87d3f4669313a8adf9cac23f05e
Miha Perosa <mihaperosa@gmail.com>
2024-01-29 15:57:24 +0100

    feat: Add swagger support

--- packages/enterprise-wallet/src/main.ts
+++ packages/enterprise-wallet/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Enterprise Wallet')
+    .setDescription('API for Enterprise Wallet')
+    .setVersion('0.1')
+    .addTag('wallet')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();

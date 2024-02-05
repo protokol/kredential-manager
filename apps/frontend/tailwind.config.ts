ff86f944da21c7b8ccf1fa8a63a252156e2d8d00
Muntean Gratian Catalin <gratiancatalin.muntean@gmail.com>
2024-02-05 14:37:35 +0200

    feat: Set up the theme & Button component & Display Button variants (#4)
    * feat: Set up the theme & Button component & Display Button variants
    
    * fix: Add lock file
    
    * fix: Changes after review
    
    * fix: Lock the packages
    
    ---------
    
    Co-authored-by: muntean.gm <muntean.gm@pg.com>

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss';
 
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,14 +9,81 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      slate: {
+        50: '#F8FAFC',
+        100: '#F1F5F9',
+        200: '#E2E8F0',
+        300: '#CBD5E1',
+        400: '#94A3B8',
+        500: '#64748B',
+        600: '#475569',
+        700: '#334155',
+        800: '#1E293B',
+        900: '#0F172A',
+        950: '#020617'
+      },
+      sky: {
+        50: '#F0F9FF',
+        100: '#E0F2FE',
+        200: '#BAE6FD',
+        300: '#7DD3FC',
+        400: '#38BDF8',
+        500: '#0EA5E9',
+        600: '#0284C7',
+        700: '#0369A1',
+        800: '#075985',
+        900: '#0C4A6E',
+        950: '#082F49'
+      },
+      green: {
+        50: '#F0FDF4',
+        100: '#DCFCE7',
+        200: '#BBF7D0',
+        300: '#86EFAC',
+        400: '#4ADE80',
+        500: '#22C55E',
+        600: '#16A34A',
+        700: '#15803D',
+        800: '#166534',
+        900: '#14532D',
+        950: '#052E16'
+      },
+      red: {
+        50: '#FEF2F2',
+        100: '#FEE2E2',
+        200: '#FECACA',
+        300: '#FCA5A5',
+        400: '#F87171',
+        500: '#EF4444',
+        600: '#DC2626',
+        700: '#B91C1C',
+        800: '#991B1B',
+        900: '#7F1D1D',
+        950: '#450A0A'
+      },
+      gold: {
+        default: '#D4AF37',
+        light: 'E1C054'
+      }
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans]
+      },
+      borderWidth: {
+        1.5: '1.5px'
       }
     }
   },
   plugins: []
 };
+
 export default config;

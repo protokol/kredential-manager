59b08130fe422cb6feecbbe2bdb0a3140746d5a0
KovacZan <zan.kovi@proton.me>
2024-02-06 12:28:38 +0100

    chore: Move db source into SRC

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { dataSourceOptions } from "db/dataSource";
+import { dataSourceOptions } from "./db/dataSource";
 import { VcModule } from "./vc/vc.module";
 import { VcController } from "./vc/vc.controller";
 import { VcService } from "./vc/vc.service";
---
+++ apps/backend/src/db/dataSource.ts
@@ -0,0 +1,18 @@
+import { VerifiableCredential } from "../entities/VerifiableCredential";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "1234",
+    database: "postgres",
+    synchronize: false, // must be disabled in production
+    entities: [VerifiableCredential],
+    migrations: ["dist/src/migrations/*{.js,.ts}"],
+    migrationsTableName: "custom_migration_table",
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;

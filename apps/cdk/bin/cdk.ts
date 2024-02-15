de36af4fc389aa3327d09139a2b34b8dd9b525d0
KovacZan <zan.kovi@proton.me>
2024-02-15 16:46:37 +0100

    ci: Implement github actions deploy via IODC

--- apps/cdk/bin/cdk.ts
+++ apps/cdk/bin/cdk.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import { BackendStack } from "../lib/backend-stack";
-import { CdkStack } from "../lib/cdk-stack";
 import { DatabaseStack } from "../lib/database-stack";
 import { KeycloakStack } from "../lib/keycloak-stack";
 import { VpcClusterStack } from "../lib/vpc-cluster-stack";
@@ -8,18 +7,18 @@ import * as cdk from "aws-cdk-lib";
 import "source-map-support/register";
 
 const app = new cdk.App();
-new CdkStack(app, "CdkStack", {
-	/* If you don't specify 'env', this stack will be environment-agnostic.
-	 * Account/Region-dependent features and context lookups will not work,
-	 * but a single synthesized template can be deployed anywhere. */
-	/* Uncomment the next line to specialize this stack for the AWS Account
-	 * and Region that are implied by the current CLI configuration. */
-	// env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-	/* Uncomment the next line if you know exactly what Account and Region you
-	 * want to deploy the stack to. */
-	// env: { account: '123456789012', region: 'us-east-1' },
-	/* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// new CdkStack(app, "CdkStack", {
+/* If you don't specify 'env', this stack will be environment-agnostic.
+ * Account/Region-dependent features and context lookups will not work,
+ * but a single synthesized template can be deployed anywhere. */
+/* Uncomment the next line to specialize this stack for the AWS Account
+ * and Region that are implied by the current CLI configuration. */
+// env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+/* Uncomment the next line if you know exactly what Account and Region you
+ * want to deploy the stack to. */
+// env: { account: '123456789012', region: 'us-east-1' },
+/* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// });
 const vpcClusterStack = new VpcClusterStack(app, "VpcClusterStack", {
 	hostedZoneId: "Z08139353PYYZ63C4KDJW",
 	zoneName: "eu-dev.protokol.sh",

295bea1aa3059029a604f3c8bd6b81890c378f24
Miha Perosa <mihaperosa@gmail.com>
2024-02-19 11:26:57 +0100

    feat: Create vc dto with create method updates

--- apps/backend/src/vc/vc.controller.ts
+++ apps/backend/src/vc/vc.controller.ts
@@ -3,23 +3,64 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { VerifiableCredential } from "src/entities/VerifiableCredential";
 import { Repository } from "typeorm";
 import { Public } from "nest-keycloak-connect";
+import { CreateVcDto } from "./dto/create-vc.dto";
+import { VerifiableEducationalID } from "src/types/schema/VerifiableEducationID202311";
+import { VCRole, VCStatus } from "src/types/VC";
 
 @Controller("vc")
 export class VcController {
     constructor(
         @InjectRepository(VerifiableCredential)
         private vcRepository: Repository<VerifiableCredential>,
-    ) {}
+        // eslint-disable-next-line prettier/prettier
+    ) { }
 
     @Get()
+    @Public(true)
     async getAll(): Promise<any> {
         return this.vcRepository.find();
     }
 
     @Post()
     @Public(true)
-    async create(@Body() vc: VerifiableCredential): Promise<any> {
-        // TODO: Add logic to verify the verifiable credential
-        return this.vcRepository.save(vc);
+    create(@Body() createVcDto: CreateVcDto) {
+        console.log({ createVcDto });
+        const newCredentialData = {
+            did: "",
+            displayName: "",
+            mail: "",
+            dateOfBirth: undefined,
+            vc_data: {},
+            vc_data_signed: {},
+            type: createVcDto.type,
+            role: VCRole.STUDENT,
+            status: VCStatus.PENDING,
+        };
+
+        switch (createVcDto.type) {
+            case "VerifiableEducationID202311": {
+                const vc_data =
+                    createVcDto.data as unknown as VerifiableEducationalID;
+
+                newCredentialData.did = vc_data.credentialSubject.id;
+                newCredentialData.displayName =
+                    vc_data.credentialSubject.displayName ?? "";
+                newCredentialData.mail = vc_data.credentialSubject.mail ?? "";
+                const birthDate = vc_data.credentialSubject.dateOfBirth;
+                newCredentialData.dateOfBirth = birthDate
+                    ? new Date(birthDate)
+                    : undefined;
+                newCredentialData.vc_data = vc_data;
+
+                break;
+            }
+            default: {
+                throw new Error(`Unknown type "${createVcDto.type}"`);
+            }
+        }
+
+        const newCredential = new VerifiableCredential();
+        Object.assign(newCredential, newCredentialData);
+        this.vcRepository.save(newCredential);
     }
 }

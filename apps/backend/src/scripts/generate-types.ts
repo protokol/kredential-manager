1d860940e4e6ab0bd284c2bca157a7c54b1d5ce8
Miha Perosa <mihaperosa@gmail.com>
2024-02-19 11:24:14 +0100

    chore: Update generate types and first supported schema

--- apps/backend/src/scripts/generate-types.ts
+++ apps/backend/src/scripts/generate-types.ts
@@ -3,30 +3,28 @@ import { writeFileSync } from "fs";
 import { resolve } from "path";
 import { compile } from "json-schema-to-typescript";
 import { JSONSchema4 } from "json-schema";
-
-const schemasToGenerate = [
-    {
-        schemaPath:
-            "src/schemas/education/verifiable-education-id/2023-11/schema.json",
-        typeName: "VerifiableEducationID202311",
-    },
-];
+import { VCSupportedSchemas } from "src/types/VC";
 
 async function generateTypes() {
-    for (let index = 0; index < schemasToGenerate.length; index++) {
-        const element = schemasToGenerate[index];
-        const typePath = `src/types/${element.typeName}.ts`;
-        console.log(`Generating types for ${element.typeName}`);
-        try {
-            const schema = await $RefParser.dereference(element.schemaPath);
-            const ts = await compile(schema as JSONSchema4, element.typeName, {
-                bannerComment: "",
-            });
-            writeFileSync(resolve(typePath), ts);
-        } catch (error) {
-            console.error("Error generating types:", error);
+    for (const key in VCSupportedSchemas) {
+        if (VCSupportedSchemas.hasOwnProperty(key)) {
+            const element = VCSupportedSchemas[key];
+            const typePath = `src/types/schema/${element.typeName}.ts`;
+            console.log(`Generating types for ${element.typeName}`);
+            try {
+                const schema = await $RefParser.dereference(element.schemaPath);
+                const ts = await compile(
+                    schema as JSONSchema4,
+                    element.typeName,
+                    {
+                        bannerComment: "",
+                    },
+                );
+                writeFileSync(resolve(typePath), ts);
+            } catch (error) {
+                console.error("Error generating types:", error);
+            }
         }
     }
 }
-
 generateTypes();

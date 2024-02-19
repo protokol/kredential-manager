b5c4b00516b76d1694110a7f94734c8e81cc38df
Miha Perosa <mihaperosa@gmail.com>
2024-02-19 11:23:24 +0100

    chore: Extend verifiable credential db schema

--- apps/backend/src/entities/VerifiableCredential.ts
+++ apps/backend/src/entities/VerifiableCredential.ts
@@ -1,14 +1,58 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { VCRole, VCStatus, VCSupportedTypes } from "src/types/VC";
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class VerifiableCredential {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column("jsonb")
-    payload: Record<string, any>;
+    @Column({ type: "text", nullable: false })
+    did: string;
 
-    // Do we need this field?
-    @Column({ default: false })
-    isSigned: boolean;
+    @Column({ type: "text", nullable: true })
+    displayName: string;
+
+    @Column({ type: "text", nullable: true })
+    mail: string;
+
+    @Column({ type: "date", nullable: true })
+    dateOfBirth: Date;
+
+    @Column({ type: "jsonb", nullable: false })
+    vc_data: Record<string, any>;
+
+    @Column({ type: "jsonb", default: {}, nullable: true })
+    vc_data_signed: Record<string, any>;
+
+    @Column({
+        type: "enum",
+        enum: VCSupportedTypes,
+        nullable: false,
+    })
+    type: VCSupportedTypes;
+
+    @Column({
+        type: "enum",
+        enum: VCRole,
+        default: VCRole.STUDENT,
+    })
+    role: VCRole;
+
+    @Column({
+        type: "enum",
+        enum: VCStatus,
+        default: VCStatus.PENDING,
+    })
+    status: VCStatus;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    updated_at: Date;
 }

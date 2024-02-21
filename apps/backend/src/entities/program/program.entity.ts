15825d04c6012292f2cd5eb3cb1d29839912bc9d
Miha Perosa <mihaperosa@gmail.com>
2024-02-21 12:49:08 +0100

    feat: Implement program entity

---
+++ apps/backend/src/entities/program/program.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Course } from "../course/course.entity";
+import { Diploma } from "../diploma/diploma.entity";
+
+@Entity()
+export class Program {
+    @PrimaryGeneratedColumn()
+    program_id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    department: string;
+
+    @Column()
+    cycle: string;
+
+    @Column()
+    total_credits: number;
+
+    @OneToMany(() => Course, (course) => course.program)
+    courses: Course[];
+
+    @OneToMany(() => Diploma, (diploma) => diploma.program)
+    diplomas: Diploma[];
+}

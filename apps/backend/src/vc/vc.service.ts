0a48d35cd995e0be0a90acb3173aad9c4ec9b3e2
Miha Perosa <mihaperosa@gmail.com>
2024-02-27 15:01:19 +0100

    fix: Add type to all response

--- apps/backend/src/vc/vc.service.ts
+++ apps/backend/src/vc/vc.service.ts
@@ -86,6 +86,7 @@ export class VcService {
                 "dateOfBirth", // *
                 "status",
                 "role",
+                "type",
                 "displayName",
                 "created_at",
                 "updated_at",

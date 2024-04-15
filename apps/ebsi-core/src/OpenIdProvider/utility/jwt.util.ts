d0c41580c5712e55e98113e21d6efab6bddeda35
Miha Perosa <mihaperosa@gmail.com>
2024-04-15 11:08:59 +0200

    chore: Clean jwt util

--- apps/ebsi-core/src/OpenIdProvider/utility/jwt.util.ts
+++ apps/ebsi-core/src/OpenIdProvider/utility/jwt.util.ts
@@ -58,34 +58,3 @@ function validateClientMetadataJwksUri(clientMetadata: any): void {
         throw new Error('Expected client metadata with jwks_uri');
     }
 }
-
-// // Utility function to fetch JWKS from a URI
-// async function fetchJWKs(jwksUri: string): Promise<JWK.Key[]> {
-//     try {
-//         const response = await fetch(jwksUri);
-//         const jwks = await response.json();
-//         return jwks.keys;
-//     } catch (error) {
-//         throw new Error('Failed to fetch JWKS');
-//     }
-// }
-
-// // Utility function to validate the presence of a 'kid' in the JWT header
-// function validateJwtHeaderKid(header: JWT.Header): void {
-//     if (!header.kid) {
-//         throw new Error('No kid specified in JWT header');
-//     }
-// }
-
-// // Utility function to verify a JWT with expiration and audience checks
-// async function verifyJwtWithExpAndAudience(token: string, jwk: JWK.Key, issuer: string): Promise<void> {
-//     try {
-//         await JWT.verify(token, jwk, {
-//             issuer,
-//             complete: true,
-//             algorithms: ['RS256', 'ES256'], // Example supported algorithms
-//         });
-//     } catch (error) {
-//         throw new Error('Failed to verify JWT: ' + error.message);
-//     }
-// }

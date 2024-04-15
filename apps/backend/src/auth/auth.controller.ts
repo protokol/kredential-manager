7a00e9842a074e64e5fc46ea480c43c1d34f4ef1
Miha Perosa <mihaperosa@gmail.com>
2024-04-15 10:54:53 +0200

    feat: Authorize enpoint wired to core package

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import {
     Public,
 } from 'nest-keycloak-connect';
 import { OpenIDProviderService } from '../openId/openId.service';
 import { IssuerService } from 'src/issuer/issuer.service';
+import { AuthorizeRequest } from '@protokol/ebsi-core';
+import { JWK } from 'jose';
+
+interface JWKS {
+    keys: JWK[];
+}
 
 @Controller('')
 export class AuthController {
@@ -17,8 +23,28 @@ export class AuthController {
 
     @Get('jwks')
     @Public(true)
-    getJwks() {
+    getJwks(): JWKS {
         const keyWithKid = this.issuer.getPublicKeyJwk();
-        return { keys: [keyWithKid] };
+        return {
+            keys: [keyWithKid]
+        };
+    }
+
+    @Get('authorize')
+    @Public(true)
+    async authorize(
+        @Query() req: AuthorizeRequest,
+        @Res() res: Response,
+    ) {
+        try {
+            const { header, redirectUrl } = await this.provider.verifyAuthorizatioAndReturnIdTokenRequest(req)
+            for (const [key, value] of Object.entries(header)) {
+                res.setHeader(key, value);
+            }
+            return res.redirect(302, redirectUrl);
+        } catch (error) {
+            console.log(error.message)
+            return res.status(400).json({ message: error.message });
+        }
     }
 }

f85a95ec7059072f13c1b1f6e7ee14289ad54755
Miha Perosa <mihaperosa@gmail.com>
2024-05-17 14:32:48 +0200

    chore: Core lib bump

--- apps/backend/src/openId/openId.service.ts
+++ apps/backend/src/openId/openId.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { OpenIdProvider, generateDidFromPrivateKey, getOpenIdConfigMetadata, getOpenIdIssuerMetadata } from '@probeta/mp-core';
 
 
-const HOST = process.env.ISSUER_BASE_URL || 'localhost:3000'; // The host of the application
+
 
 
 @Injectable()
 export class OpenIDProviderService {
     private provider: OpenIdProvider;
-
     constructor() {
+        const HOST = process.env.ISSUER_BASE_URL || 'localhost:3000';
         const privateKeyID = process.env.ISSUER_PRIVATE_KEY_ID;
         const issuerMetadata = getOpenIdIssuerMetadata(HOST);
         const configMetadata = getOpenIdConfigMetadata(HOST);

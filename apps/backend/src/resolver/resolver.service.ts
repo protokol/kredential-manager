84412560c9670ae416ad3892e46179d176bb353e
Miha Perosa <mihaperosa@gmail.com>
2024-06-21 13:51:22 +0200

    feat: Implement DID resolver service

--- apps/backend/src/resolver/resolver.service.ts
+++ apps/backend/src/resolver/resolver.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from "@nestjs/common";
-// import { DIDDocument, Resolver } from "did-resolver";
-// import { getResolver as getDidWebResolver } from "web-did-resolver";
+import { DIDDocument, Resolver } from "did-resolver";
+import { getResolver as getDidWebResolver } from "web-did-resolver";
+import * as KeyDidResolver from 'key-did-resolver'
 // const { getDidKeyResolver } = require("@veramo/did-provider-key");
-// import { getResolver } from "@cef-ebsi/key-did-resolver";
+import { getResolver } from "@cef-ebsi/key-did-resolver";
+import { DIDResolutionResult } from "./DIDDocument.interface";
 // import type { getResolver } from "@cef-ebsi/key-did-resolver";
 // import { getDidKeyResolver } from "@veramo/did-provider-key";
-
+import { JWK } from '@probeta/mp-core';
 @Injectable()
 export class ResolverService {
-    async resolveDID(did: string) {
-        // const resolver = new Resolver({
-        //     // ...getDidKeyResolver(),
-        //     ...getDidWebResolver(),
-        //     ...getResolver(),
-        // });
-        // const resolvedDid = await resolver.resolve(did);
-        // return resolvedDid;
+    async resolveDID(did: string): Promise<JWK> {
+        const keyDidResolver = KeyDidResolver.getResolver()
+        const resolver = new Resolver({
+            ...keyDidResolver,
+            ...getDidWebResolver(),
+            ...getResolver(),
+        });
+        const resolvedDid = await resolver.resolve(did);
+        const publicKey = this.findPublicKeyById(resolvedDid as DIDResolutionResult, did)
+        return publicKey;
+    }
+
+    findPublicKeyById(didResolutionResult: DIDResolutionResult, verificationMethodId: string): JWK {
+        const { didDocument } = didResolutionResult;
+        const verificationMethod = didDocument.verificationMethod.find(method => method.id.length && method.id.startsWith(verificationMethodId));
+        if (!verificationMethod) {
+            console.log("Verification method not found for the given ID.");
+            return null;
+        }
+        return verificationMethod.publicKeyJwk as JWK;
     }
 }

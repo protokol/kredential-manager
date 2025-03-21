8e4786b22ea0127751a39d99ca6f31804a6fdff6
Miha Perosa <mihaperosa@gmail.com>
2025-03-21 11:27:49 +0100

    refactor: Add dynamic scope discovery

--- apps/backend/src/openId/openId.service.ts
+++ apps/backend/src/openId/openId.service.ts
@@ -1,19 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { OpenIdConfiguration, OpenIdIssuer, OpenIdProvider } from '@protokol/kredential-core';
-import { EnterpriseJwtUtil } from '../issuer/jwt.util';
-import { SchemaTemplateService } from 'src/schemas/schema-template.service';
-import { PresentationDefinitionService } from 'src/presentation/presentation-definition.service';
-import { PresentationDefinition } from '@entities/presentation-definition.entity';
+import { Injectable } from "@nestjs/common";
+import {
+    OpenIdConfiguration,
+    OpenIdIssuer,
+    OpenIdProvider,
+} from "@protokol/kredential-core";
+import { EnterpriseJwtUtil } from "../issuer/jwt.util";
+import { SchemaTemplateService } from "src/schemas/schema-template.service";
+import { PresentationDefinitionService } from "src/presentation/presentation-definition.service";
+import { PresentationDefinition } from "@entities/presentation-definition.entity";
 
-
-export function getOpenIdConfigMetadata(baseUrl: string, presentationDefinitions: PresentationDefinition[]): OpenIdConfiguration {
+export function getOpenIdConfigMetadata(
+    baseUrl: string,
+    presentationDefinitions: PresentationDefinition[],
+): OpenIdConfiguration {
+    const scopesSupported = presentationDefinitions.map((def) => def.scope);
     return {
         redirect_uris: [`${baseUrl}/direct_post`],
         issuer: `${baseUrl}`,
         authorization_endpoint: `${baseUrl}/authorize`,
         token_endpoint: `${baseUrl}/token`,
         jwks_uri: `${baseUrl}/jwks`,
-        scopes_supported: ["openid"],
+        scopes_supported: ["openid", ...scopesSupported],
         response_types_supported: ["code", "id_token", "vp_token"],
         response_modes_supported: ["query"],
         grant_types_supported: ["authorization_code"],
@@ -25,35 +32,40 @@ export function getOpenIdConfigMetadata(baseUrl: string, presentationDefinitions
         token_endpoint_auth_methods_supported: ["private_key_jwt"],
         vp_formats_supported: {
             jwt_vp: {
-                alg_values_supported: ["ES256"]
+                alg_values_supported: ["ES256"],
             },
             jwt_vc: {
-                alg_values_supported: ["ES256"]
-            }
+                alg_values_supported: ["ES256"],
+            },
         },
         subject_syntax_types_supported: ["did:key", "did:ebsi"],
         subject_trust_frameworks_supported: ["ebsi"],
-        id_token_types_supported: [
-        ],
-        presentation_definitions_supported: presentationDefinitions.map(def => ({
-            id: def.definition.id,
-            format: def.definition.format,
-            constraints: def.definition.input_descriptors.map(descriptor => ({
-                id: descriptor.id,
-                format: descriptor.format,
-                constraints: descriptor.constraints
-            }))
-        }))
+        id_token_types_supported: [],
+        presentation_definitions_supported: presentationDefinitions.map(
+            (def) => ({
+                id: def.definition.id,
+                format: def.definition.format,
+                constraints: def.definition.input_descriptors.map(
+                    (descriptor) => ({
+                        id: descriptor.id,
+                        format: descriptor.format,
+                        constraints: descriptor.constraints,
+                    }),
+                ),
+            }),
+        ),
     };
 }
 
-export async function getOpenIdIssuerMetadata(baseUrl: string): Promise<OpenIdIssuer> {
+export async function getOpenIdIssuerMetadata(
+    baseUrl: string,
+): Promise<OpenIdIssuer> {
     return {
         credential_issuer: `${baseUrl}`,
         authorization_server: `${baseUrl}/authorize`,
         credential_endpoint: `${baseUrl}/credential`,
         deferred_credential_endpoint: `${baseUrl}/credential_deferred`,
-        credentials_supported: []
+        credentials_supported: [],
     };
 }
 @Injectable()
@@ -62,7 +74,10 @@ export class OpenIDProviderService {
     private jwtUtil: EnterpriseJwtUtil;
     private initialized: boolean = false;
 
-    constructor(private schemaTemplateService: SchemaTemplateService, private presentationDefinitionService: PresentationDefinitionService) { }
+    constructor(
+        private schemaTemplateService: SchemaTemplateService,
+        private presentationDefinitionService: PresentationDefinitionService,
+    ) {}
 
     private async init() {
         if (this.initialized) return;
@@ -70,36 +85,57 @@ export class OpenIDProviderService {
     }
 
     private async initializeProvider() {
-        const HOST = process.env.ISSUER_BASE_URL || 'localhost:3000';
+        const HOST = process.env.ISSUER_BASE_URL || "localhost:3000";
 
-        const credentialsSupported = await this.schemaTemplateService.getCredentialsSupported();
-        const presentationDefinitions = await this.presentationDefinitionService.findAll();
+        const credentialsSupported =
+            await this.schemaTemplateService.getCredentialsSupported();
+
+        console.log({ credentialsSupported });
+        const presentationDefinitions =
+            await this.presentationDefinitionService.findAll();
         const baseMetadata = await getOpenIdIssuerMetadata(HOST);
         const issuerMetadata = {
             ...baseMetadata,
-            credentials_supported: credentialsSupported
+            credentials_supported: credentialsSupported,
         };
-        const configMetadata = getOpenIdConfigMetadata(HOST, presentationDefinitions);
 
+        const configMetadata = getOpenIdConfigMetadata(
+            HOST,
+            presentationDefinitions,
+        );
 
-        if (!process.env.ISSUER_PRIVATE_KEY_JWK || !process.env.ISSUER_PRIVATE_KEY_ID || !process.env.ISSUER_PUBLIC_KEY_JWK) {
-            throw new Error('Missing required environment variables for issuer');
+        console.log({ configMetadata });
+        console.log({ presentationDefinitions });
+
+        if (
+            !process.env.ISSUER_PRIVATE_KEY_JWK ||
+            !process.env.ISSUER_PRIVATE_KEY_ID ||
+            !process.env.ISSUER_PUBLIC_KEY_JWK
+        ) {
+            throw new Error(
+                "Missing required environment variables for issuer",
+            );
         }
 
         try {
             const privateKeyJwk = {
                 alg: "ES256",
                 kid: process.env.ISSUER_PRIVATE_KEY_ID,
-                ...JSON.parse(process.env.ISSUER_PRIVATE_KEY_JWK || '{}')
+                ...JSON.parse(process.env.ISSUER_PRIVATE_KEY_JWK || "{}"),
             };
 
             this.jwtUtil = new EnterpriseJwtUtil(privateKeyJwk);
 
-            this.provider = new OpenIdProvider(issuerMetadata, configMetadata, privateKeyJwk, this.jwtUtil);
+            this.provider = new OpenIdProvider(
+                issuerMetadata,
+                configMetadata,
+                privateKeyJwk,
+                this.jwtUtil,
+            );
             this.initialized = true;
         } catch (error) {
-            console.error('Error parsing JWKs:', error);
-            throw new Error('Error parsing JWKs');
+            console.error("Error parsing JWKs:", error);
+            throw new Error("Error parsing JWKs");
         }
     }
     async getInstance(): Promise<OpenIdProvider> {
